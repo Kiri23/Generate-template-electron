6cea24dbc4f93b32395771f50ab1addea125ef7e
kiri23 <christian@opticpower.com>
2023-04-14 02:17:38 -0400

    more styling, heading , tooltip, animation

--- frontend/src/MainPage.tsx
+++ frontend/src/MainPage.tsx
@@ -1,71 +1,123 @@
-import React, { useState } from 'react';
-import { Box, Heading, VStack, List, ListItem, Input, Button, Icon, Text } from '@chakra-ui/react';
-import { CheckIcon, AttachmentIcon } from '@chakra-ui/icons';
-import './mainPage.css';
+import {
+  Button,
+  VStack,
+  Box,
+  Text,
+  Heading,
+  Input,
+  List,
+  ListItem,
+  Icon,
+  useToast,
+  Collapse,
+  Tooltip
+} from "@chakra-ui/react";
+import { CheckIcon, AttachmentIcon, WarningIcon } from "@chakra-ui/icons";
+import React, { useState } from "react";
 
-const MainPage: React.FC = () => {
-  const templates = [
-    'SlackBot',
-    'FastAPI',
-    'CLI tool',
-  ];
+const templates = ["SlackBot", "FastAPI", "CLI tool"];
+
+const MainPage = () => {
   const [selectedTemplate, setSelectedTemplate] = useState<string | null>(null);
-  const [selectedPath, setSelectedPath] = useState<string | null>(null);
+  const [destination, setDestination] = useState<string | null>(null);
+  const toast = useToast();
 
   const handleTemplateClick = (template: string) => {
-      setSelectedTemplate((prevSelectedTemplate) =>
-        prevSelectedTemplate === template ? "" : template
+    setSelectedTemplate((prevSelectedTemplate) =>
+      prevSelectedTemplate === template ? null : template
     );
   };
 
-  const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files[0]) {
-      setSelectedPath(event.target.files[0].path);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setDestination(e.target.files[0].path);
     }
   };
 
-  const handleDuplicate = () => {
-    // Logic for duplicating the template in the selected destination
+  const handleChooseDestinationClick = () => {
+    document.getElementById("destination-input")?.click();
+  };
+
+  const handleDuplicateClick = () => {
+    toast({
+      title: "Template duplicated",
+      description: `Template ${selectedTemplate} has been duplicated to ${destination}`,
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+    });
   };
 
   return (
-    <Box className="container">
-      <Text fontSize="2xl" fontWeight="bold" className="title">
-        Select the template you want to duplicate
-      </Text>
-      <VStack as="ul" className="list" spacing={3}>
-      <List className="list" spacing={3}>
-  {templates.map((template) => (
+    <VStack
+      alignItems="center"
+      justifyContent="center"
+      spacing={10}
+      minHeight="100vh"
+      className="main-page"
+    >
+      <VStack alignItems="center" spacing={4}>
+        <Heading as="h1" size="2xl">
+          Template Duplicator
+        </Heading>
+        <Text fontSize="lg" textAlign="center">
+          Select a project template and choose a destination to create a duplicate. Get started with a pre-configured template for SlackBot, FastAPI, CLI tool, and more.
+        </Text>
+      </VStack>
+      <Box>
+        <Text fontSize="xl" mb={4}>
+          Select the template you want to duplicate
+        </Text>
+        <List spacing={4}>
+          {templates.map((template) => (
             <ListItem
-              as="li"
               key={template}
+              p={4}
+              borderWidth={1}
+              borderRadius="lg"
+              borderColor="gray.300"
+              _hover={{ bg: "gray.100" }}
+              cursor="pointer"
+              className={`list-item ${selectedTemplate === template ? "selected" : ""}`}
               onClick={() => handleTemplateClick(template)}
-              className={`list-item ${selectedTemplate === template ? 'selected' : ''}`}
             >
               {template}
-          {selectedTemplate === template && <Icon as={CheckIcon} ml={2} className="checkmark"/>}
+              <Collapse in={selectedTemplate === template} animateOpacity>
+                <Icon as={CheckIcon} ml={2} />
+              </Collapse>
             </ListItem>
           ))}
         </List>
-      </VStack>
-      <Box className="input-wrapper">
-        <Input
-          type="file"
-          id="file"
-          className="input"
-          onChange={handleFileInput}
-        />
-        <label htmlFor="file" className="input-label">
-          <Icon as={AttachmentIcon} />
-          <span className="input-label-text">
-            {selectedPath ? 'Path selected' : 'Choose a destination'}
-          </span>
-        </label>
       </Box>
-      <Button onClick={handleDuplicate} className="button">
-        Duplicate
+      <Button
+        leftIcon={<AttachmentIcon />}
+        onClick={handleChooseDestinationClick}
+        colorScheme="blue"
+        className="button"
+      >
+        Choose destination
       </Button>
-    </Box>
+      <Input
+        type="file"
+        accept="directory"
+        onChange={handleFileChange}
+        display="none"
+        id="destination-input"
+      />
+      <Button
+        leftIcon={<CheckIcon />}
+        onClick={handleDuplicateClick}
+        isDisabled={selectedTemplate === null || destination === null}
+        colorScheme="blue"
+        className="button" >
+        Duplicate
+        {selectedTemplate === null || destination === null ? (
+          <Tooltip label="Please select a template and a destination" placement="top">
+            <Icon as={WarningIcon} ml={2} />
+          </Tooltip>
+        ) : null}
+      </Button>
+    </VStack>
   );
 };
 
