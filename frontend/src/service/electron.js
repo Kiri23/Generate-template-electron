f191af20694eb38e081618d636c1ffd7074551eb
kiri23 <christian@opticpower.com>
2023-04-17 17:34:45 -0400

    add comment explaining the guard

--- frontend/src/service/electron.js
+++ frontend/src/service/electron.js
@@ -1,5 +1,7 @@
 let send = () => {};
 let on = () => {};
+// If we are in the electron environment, we can use the electronAPI.
+// otherwise, don't throw an error on the the web browser.
 if (window.electronAPI) {
   send = window.electronAPI.send;
   on = window.electronAPI.on;
